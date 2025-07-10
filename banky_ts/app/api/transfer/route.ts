489a6135eaec7ff543bdeccc1fd2e737bacb00a6
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-07-10 16:47:08 +0100

    from was ot showing where the money dey come from soo i added it

--- banky_ts/app/api/transfer/route.ts
+++ banky_ts/app/api/transfer/route.ts
@@ -62,6 +62,7 @@ export async function POST(req: NextRequest){
         type: 'transfer',
         amount: amt,
         to: phone,
+        from: sender.phone,
         })
 
         receiver.transactions.push({
